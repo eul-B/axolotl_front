58bc23cbae5c3287b0d6abfb2fc01b2a3a0a156d
eulB <wkdjs1203@naver.com>
2023-05-25 00:10:04 +0900

    4th commit

--- src/components/Heatmap/heatmap.js
+++ src/components/Heatmap/heatmap.js
@@ -1,49 +1,104 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react';
 import Chart from "react-apexcharts";
+import axios from 'axios';
 
-const MyCharts = () => {
-  const [averageTemp, setAverageTemp] = useState([]);
-  const [date, setDate] = useState([]);
+
+  const MyCharts = () => {
+    const [averageTemp, setAverageTemp] = useState([]);
+    const [date, setDate] = useState([]);
 
   useEffect(() => {
-    const getData = async () => {
-    const url = 'http://localhost:8000/temperature';
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-      setAverageTemp(data?.map((item) => item.average_temp));
-      setDate(data?.map((item) => item.date));
-    } catch (error) {
-        console.log(error);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/temperature');
+        if (Array.isArray(response.data)) {
+          const modifiedData = response.data.map((item) => item.date);
+          const modifiedTmp = response.data.map((item) => item.average_temp);
+          setDate(modifiedData);
+          setAverageTemp(modifiedTmp);
+        } else {
+          setDate([]);
+          setAverageTemp([]);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+
+    const interval = setInterval(() => {
+      fetchData();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []); 
+
+    // useEffect(() => {
+    //   const getData = async () => {
+    //   const url = 'http://localhost:8000/temperature';
+    //   try {
+    //     const response = await fetch(url);
+    //     const data = await response.json();
+    //     console.log(data);
+    //     setAverageTemp(data?.map((item) => item.average_temp));
+    //     // setDate(data?.map((item) => item.date));
+    //   } catch (error) {
+    //       console.log(error);
+    //   }
+    // };
+    //   getData();
+    // }, []);
+  
+   const series = [ //data on the y-axis
+      {
+        name: "",
+        data: averageTemp
+      },{
+        name: "",
+        data: averageTemp
+      },{
+        name: "",
+        data: averageTemp
+      }
+    ];
+    const options = { //data on the x-axis
+    chart: { id: 'bar-chart'},
+    xaxis: {
+      categories: date
+    },
+    dataLabels: {
+      enabled: false
+    },
+    colors:["#000000", "#FF0000", "#0000FF"],
+    grid:{
+      show:false
+    },
+    stroke:{
+      width:0
     }
   };
-    getData();
-  }, []);
-
- const series = [ //data on the y-axis
-    {
-      name: "Temperature in Celsius",
-      data: averageTemp
-    }
-  ];
-  const options = { //data on the x-axis
-  chart: { id: 'bar-chart'},
-  xaxis: {
-    categories: date
-  }
-};
-
-  return (
-    <div>
-      <Chart
-        options={options}
-        series={series}
-        type="heatmap"
-        width="450"
-      />
+  
+    return (
+      <div>
+        <Chart
+          options={options}
+          series={series}
+          type="heatmap"
+          width="850"
+          height="700"
+          
+        />
+         <div>
+          {/* {JSON.stringify(date)} */}
+          
+      {/* { result.isLoading && 'loading' }
+      { result.error && 'error' }
+      { result.data && result.data.name } */}
     </div>
-  )
-}
-
-export default MyCharts;
+      </div>
+    )
+  }
+  
+  export default MyCharts;
